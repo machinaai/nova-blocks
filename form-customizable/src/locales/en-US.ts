b1df56e91c872e2cdf27e1a78dc3545f5d8a000a
Francisco Garcia <francisco.garcia@ids.com.mx>
2020-08-11 17:04:29 -0500

    feat: make a new form customizable with item form

---
+++ form-customizable/src/locales/en-US.ts
@@ -0,0 +1,10 @@
+export default {
+  'formCustom.input.check': 'Verify information',
+  'formCustom.continue.button': 'Continue',
+  'formCustom.input.empty': 'Introduce information',
+  'formCustom.validate.button': 'Validate',
+  'formCustom.label.cancel': 'Cancel',
+  'formCustom.label.itsnome': 'Its no me',
+  'formCustom.placeholder.input1': 'User',
+  'formCustom.placeholder.input2': 'Password',
+};
