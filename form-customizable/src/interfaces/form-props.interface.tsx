4fe10b289fc27fdc41d9e839d213ba45482c5431
Francisco Garcia <francisco.garcia@ids.com.mx>
2020-08-12 16:48:34 -0500

    refactor: add new functionality at form

---
+++ form-customizable/src/interfaces/form-props.interface.tsx
@@ -0,0 +1,33 @@
+import { InputProps } from './input-auto-label.interface';
+/*
+ * Interface for the props of customizable form
+ */
+export interface FormProps {
+  /*
+   * Property of fields
+   */
+  formFields?: InputProps[];
+  /*
+   * Value of fields in form
+   */
+  valueFields?: any;
+  /*
+   * Property submit of form event
+   */
+  onSumbit?: ActionInterface;
+  /*
+   * Property cancel of form event
+   */
+  onCancel?: ActionInterface;
+  /*
+   * Property return of form event
+   */
+  onReturn?: ActionInterface;
+}
+  /*
+   * Propertys of events
+   */
+interface ActionInterface {
+  label: string;
+  action: Function;
+}
