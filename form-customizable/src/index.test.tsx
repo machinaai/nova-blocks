5b4b18994d09e72b7df342d78e96862640a92342
Francisco Garcia <francisco.garcia@ids.com.mx>
2020-08-17 11:24:28 -0500

    test: complete unit test form customizable

--- form-customizable/src/index.test.tsx
+++ form-customizable/src/index.test.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import FormCustomizable from "./index";
 import { Link } from "umi";
-import { Button } from "antd";
+import { Button, Form } from "antd";
 import FormItem from "./FormItem/index";
+import LoginItem from "./FormItem/index";
 const LinkComponent = Link;
 
 const renderComponent = (props?: any) => {
@@ -21,11 +22,22 @@ describe("Input", () => {
   });
 
   it("Should be create a snapshot with props", () => {
-    const props = {
-      valueFields: () => {},
-    };
+    const props = {};
 
     const component = renderComponent(props);
+    component.update();
+    expect(component).toMatchSnapshot();
+  });
+
+  it("Should be create a snapshot with props", () => {
+    const props = {
+      valueFields: {
+        username: "Nova solutions",
+      },
+    };
+
+    const component = mount(<FormCustomizable {...props} />);
+    component.update();
     expect(component).toMatchSnapshot();
   });
 
@@ -65,5 +77,46 @@ describe("Input", () => {
     });
   });
 
-  
+  describe("onValidateForm", () => {
+    it("should call onValidateForm and show button", () => {
+      let component;
+      const props = {
+        onSumbit: {
+          label: "validate",
+          action: () => {},
+        },
+      };
+
+      component = mount(<FormCustomizable {...props} />);
+      component
+        .find("input")
+        .simulate("change", { target: { value: "username" } });
+
+      const actionFunction = jest.spyOn(props.onSumbit, "action");
+      component.find(Button).at(0).simulate("click");
+      component.update();
+      expect(actionFunction).toBeCalled();
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe("validateForm", () => {
+    it("should call reset fields before form is finish", async () => {
+      let component;
+      const props = {
+        onSumbit: {
+          label: "validate",
+          action: () => {},
+        },
+      };
+
+      component = renderComponent(props);
+      // const form = component.find(Form).prop('form');
+      // console.log(form);
+      // component.find(Form).prop("form");
+      component.find(Form).prop("onFinish")();
+
+      expect(component).toMatchSnapshot();
+    });
+  });
 });
