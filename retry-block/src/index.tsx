3f72f57988c7dd515e9a4e9ed832565f244323b8
Fernando Roman <froman@ids.com.mx>
2020-10-07 18:19:11 -0500

    feature: create retry block

---
+++ retry-block/src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './index.css';
+import { RedoOutlined } from '@ant-design/icons';
+
+interface Props {
+  onClick?: Function,
+  message: string,
+  iconSize?: number,
+  fontSize?: number
+}
+
+const RetryBlock: React.FC<Props> = ({onClick, message = 'Volver intentar', iconSize = 50, fontSize = 16}) => {
+
+  const onRetry = () => (onClick && onClick());
+
+  return (
+    <>
+      <div className={styles.error}>
+        <RedoOutlined style={{fontSize: `${iconSize}px`}}/>
+        <p onClick={onRetry} className={styles.errorText} style={{fontSize: `${fontSize}px`}}> { message } </p>
+      </div>
+    </>
+  );
+}
+
+export default RetryBlock;
