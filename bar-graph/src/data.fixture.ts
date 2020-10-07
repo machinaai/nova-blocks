56a3e8da0cb9a04fa7595cda9ef533366c3a3a69
Fernando Roman <froman@ids.com.mx>
2020-10-07 12:26:59 -0500

    feat: create bar braph block

---
+++ bar-graph/src/data.fixture.ts
@@ -0,0 +1,24 @@
+import { DataInterface } from './data.interface';
+
+export const dataFixture: DataInterface[] = [
+    {
+        text: '1992',
+        value: 300
+    },
+    {
+        text: '1993',
+        value: -300
+    },
+    {
+        text: '1991',
+        value: 192
+    },
+    {
+        text: '1995',
+        value: 926
+    },
+    {
+        text: '1990',
+        value: 496
+    }
+]
---
+++ bar-graph/src/data.interface.ts
@@ -0,0 +1,4 @@
+export interface DataInterface {
+    text: string,
+    value: number,
+}
