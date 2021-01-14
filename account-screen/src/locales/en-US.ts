510adabc3ff85bf61921861deca4b67b3c3f756f
Sandra <samontiel@novasolutionsystems.com>
2021-01-14 16:41:55 -0600

    feat():add account-screen problock

---
+++ account-screen/src/locales/en-US.ts
@@ -0,0 +1,11 @@
+export default {
+  'rao-account-screen.H_title': 'Congratulations',
+  'rao-account-screen.H_subtitle': 'Your virtual card has been activated, you can use it now.',
+  'rao-account-screen.B_desc1': 'Your virtual card number is:',
+  'rao-account-screen.B_desc2': 'We have sent you all the details of your account via WhatsApp',
+  'rao-account-screen.text_btn': 'It is understood',
+  'rao-account-screen.F_title1': 'Download our application at:',
+  'rao-account-screen.F_title2': 'Download our app in the store',
+  'rao-account-screen.wallet': 'Add your card to your wallet',
+  'rao-account-screen.wallet_btn': 'Add to wallet'
+}
