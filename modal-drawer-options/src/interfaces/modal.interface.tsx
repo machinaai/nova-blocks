283e6bbb0d692b3d8552e99804c5bba42a0ae336
Sandra <samontiel@novasolutionsystems.com>
2021-01-15 12:06:58 -0600

    fix():modify some styles for the block

--- modal-drawer-options/src/interfaces/modal.interface.tsx
+++ modal-drawer-options/src/interfaces/modal.interface.tsx
@@ -2,15 +2,16 @@ export interface ModalProps {
     title?: string,
     subtitle?: string,
     options?: ItemOptions[],
+    backColorOp?:string,
     fontFamily?:Fonts,
     onlyModal?:boolean,
     onlyDrawer?:boolean,
     onClose?:Function | any
 }
 export interface ItemOptions {
-    icon: any,
+    icon: string | React.ReactNode,
     valOption: string,
-    action:Function|any,
+    action:any,
     color?:string
 }
 interface Fonts{
