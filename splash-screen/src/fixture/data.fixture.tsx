0304800f6628b5a2012f90da037cb7282a74c0da
lorena <lzamorano@novasolutionsystems.com>
2021-01-22 13:11:46 -0600

    feat: change routes of images to url

--- splash-screen/src/fixture/data.fixture.tsx
+++ splash-screen/src/fixture/data.fixture.tsx
@@ -1,9 +1,9 @@
-import circle from "./multmedia/circle-bg.png";
-import logo from "./multmedia/Novabank.svg";
-import logoMobile from "./multmedia/Novabank-mobile.svg";
-import robot01 from "./multmedia/Robot-01.gif";
-import robot02 from "./multmedia/Robot-02.gif";
-import robot03 from "./multmedia/Robot-03.gif";
+import circle from "https://raw.githubusercontent.com/machinaai/nova-blocks/feature/splash-screen/splash-screen/src/fixture/multimedia/circle-bg.png";
+import logo from "https://raw.githubusercontent.com/machinaai/nova-blocks/f1f04cee27dbc7c65ee0b4c89d314d8ac57cd538/splash-screen/src/fixture/multimedia/Novabank.svg";
+import logoMobile from "https://raw.githubusercontent.com/machinaai/nova-blocks/f1f04cee27dbc7c65ee0b4c89d314d8ac57cd538/splash-screen/src/fixture/multimedia/Novabank-mobile.svg";
+import robot01 from "https://raw.githubusercontent.com/machinaai/nova-blocks/feature/splash-screen/splash-screen/src/fixture/multimedia/Robot-01.gif";
+import robot02 from "https://raw.githubusercontent.com/machinaai/nova-blocks/feature/splash-screen/splash-screen/src/fixture/multimedia/Robot-02.gif";
+import robot03 from ".https://raw.githubusercontent.com/machinaai/nova-blocks/feature/splash-screen/splash-screen/src/fixture/multimedia/Robot-03.gif";
 
 export const dataImages = {
   backgroundImage:circle,
