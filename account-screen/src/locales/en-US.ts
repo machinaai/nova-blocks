acecac8cfdb2a1aae333b815264d122bbebe5364
Sandra <samontiel@novasolutionsystems.com>
2021-01-25 16:56:25 -0600

    fix():change the id of locales

--- account-screen/src/locales/en-US.ts
+++ account-screen/src/locales/en-US.ts
@@ -1,11 +1,11 @@
 export default {
-  'rao-account-screen.H_title': 'Congratulations',
-  'rao-account-screen.H_subtitle': 'Your virtual card has been activated, you can use it now.',
-  'rao-account-screen.B_desc1': 'Your virtual card number is:',
-  'rao-account-screen.B_desc2': 'We have sent you all the details of your account via WhatsApp',
-  'rao-account-screen.text_btn': 'It is understood',
-  'rao-account-screen.F_title1': 'Download our application at:',
-  'rao-account-screen.F_title2': 'Download our app in the store',
-  'rao-account-screen.wallet': 'Add your card to your wallet',
-  'rao-account-screen.wallet_btn': 'Add to wallet'
+  'accountScreen.H_title': 'Congratulations',
+  'accountScreen.H_subtitle': 'Your virtual card has been activated, you can use it now.',
+  'accountScreen.B_desc1': 'Your virtual card number is:',
+  'accountScreen.B_desc2': 'We have sent you all the details of your account via WhatsApp',
+  'accountScreen.text_btn': 'It is understood',
+  'accountScreen.F_title1': 'Download our application at:',
+  'accountScreen.F_title2': 'Download our app in the store',
+  'accountScreen.wallet': 'Add your card to your wallet',
+  'accountScreen.wallet_btn': 'Add to wallet'
 }
--- account-screen/src/locales/es-ES.ts
+++ account-screen/src/locales/es-ES.ts
@@ -1,11 +1,11 @@
 export default {
-  'rao-account-screen.H_title': 'Felicidades',
-  'rao-account-screen.H_subtitle': 'Tu tarjeta virtual ha sido activada, ya puedes utilizarla.',
-  'rao-account-screen.B_desc1': 'Tu número de tarjeta virtual es:',
-  'rao-account-screen.B_desc2': 'Te hemos enviado todo los detalles de tu cuenta vía WhatsApp',
-  'rao-account-screen.text_btn': 'Entendido',
-  'rao-account-screen.F_title1': 'Descarga nuestra aplicación en:',
-  'rao-account-screen.F_title2': 'Descarga nuestra aplicación en la tienda',
-  'rao-account-screen.wallet': 'Agrega tu tarjeta a tu wallet',
-  'rao-account-screen.wallet_btn': 'Agregar a wallet'
+  'accountScreen.H_title': 'Felicidades',
+  'accountScreen.H_subtitle': 'Tu tarjeta virtual ha sido activada, ya puedes utilizarla.',
+  'accountScreen.B_desc1': 'Tu número de tarjeta virtual es:',
+  'accountScreen.B_desc2': 'Te hemos enviado todo los detalles de tu cuenta vía WhatsApp',
+  'accountScreen.text_btn': 'Entendido',
+  'accountScreen.F_title1': 'Descarga nuestra aplicación en:',
+  'accountScreen.F_title2': 'Descarga nuestra aplicación en la tienda',
+  'accountScreen.wallet': 'Agrega tu tarjeta a tu wallet',
+  'accountScreen.wallet_btn': 'Agregar a wallet'
 };
