86aee1a3b17488e0d041ffba8051eb5860e09179
lorena <lzamorano@novasolutionsystems.com>
2021-01-26 13:28:54 -0600

    fix(used-channels): remove dependency dataset - update snapshot

--- used-channels/src/helpers/getPercentage.tsx
+++ used-channels/src/helpers/getPercentage.tsx
@@ -4,15 +4,18 @@ export const getPercentage = (data: any) => {
   if (data) {
     let dataDevice: DataChart[] = [];
     const totalDev = data.mobile + data.web + data.tablet;
+    
     for (const property in data) {
       dataDevice = [
         ...dataDevice,
         {
           type: property,
           value: Number(((100 * data[property]) / totalDev).toFixed(2)),
+          percent: Number(((100 * data[property]) / totalDev).toFixed(2))/100,
         },
       ];
     }
+
     return dataDevice;
   }
 
@@ -20,14 +23,17 @@ export const getPercentage = (data: any) => {
     {
       type: 'mobile',
       value: 0,
+      percent: 0,
     },
     {
       type: 'web',
       value: 0,
+      percent: 0,
     },
     {
       type: 'tablet',
       value: 0,
+      percent: 0,
     },
   ];
 };
